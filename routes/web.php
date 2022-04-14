8d23fbe1df80eac34c878fe708a04533d3384960
lestariypt <putulestari42@gmail.com>
2022-04-14 19:38:03 +0800

    menambahkan di web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 Auth::routes();
 
@@ -54,7 +54,3 @@ Route::get('/hapus_peminjaman/{id}', 'DataController@hapus_peminjaman');
 // User
 Route::get('/ubah_user', 'DataController@ubah_user')->name('ubah_user');
 Route::post('/proses_edit_user', 'DataController@proses_edit_user')->name('proses_edit_user');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
